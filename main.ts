10f8912068845cf12b4aaf994e43e907d506bc94
JulPreu <julian.preuschoff@gmx.net>
2026-07-01 17:25:28 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,37 @@
+function addition () {
+    Zahl1 = randint(1, 50)
+    Zahl2 = randint(1, 50)
+    Aufgabe = "Was ist" + Zahl1 + "+" + Zahl2 + "?"
+    Antwort = 1
+}
+let Antwort = 0
+let Aufgabe = ""
+let Zahl2 = 0
+let Zahl1 = 0
+let Rechenart = 0
+while (input.pinIsPressed(TouchPin.P2) == false) {
+    if (input.buttonIsPressed(Button.B)) {
+        Rechenart += 1
+    } else if (input.buttonIsPressed(Button.A)) {
+        Rechenart += -1
+    }
+    if (Rechenart > 4 || Rechenart < 1) {
+        Rechenart = 1
+    }
+    if (Rechenart == 1) {
+        basic.showString("+")
+    } else if (Rechenart == 2) {
+        basic.showString("-")
+    } else if (Rechenart == 3) {
+        basic.showString("*")
+    } else if (Rechenart == 4) {
+        basic.showString(":")
+    }
+}
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P2)) {
+        if (Rechenart == 1) {
+            addition()
+        }
+    }
+})
